898e8c25d1f81ef0c0e18073c0fa0ed77418f18b
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-05-09 04:05:21 +0900

    feat : Add models

--- models.py
+++ models.py
@@ -1,15 +1,31 @@
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 from database import Base
-from typing import Optional, List
-from pydantic import BaseModel, Field
+from sqlalchemy.orm import relationship
 
-class CreateUser(BaseModel):
-    username: str
-    email: Optional[str]
-    first_name: str
-    last_name: str
-    password: str
 
-class Routine(BaseModel):
-    title : str
-    description : Optional[str]
-    icon : str
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    routines = relationship("Routines", back_populates="owner")
+
+
+class Routines(Base):
+    __tablename__ = "routines"  # 테이블 이름 정의
+
+    # 테이블의 열을 정의
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="routines")
